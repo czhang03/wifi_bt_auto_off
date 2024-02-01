f4c5b47f66f7339217a698f5ff90290407eacc66
czhang03 <zcsx.ok@outlook.com>
2024-01-31 20:01:10 -0500

    less echo

--- install.sh
+++ install.sh
@@ -7,19 +7,15 @@ set -e
 cd /tmp/
 
 # download the script
-echo "downloading script..."
 wget -q https://raw.githubusercontent.com/czhang03/wifi_bt_auto_off/main/wifi_bt_auto_off.py
 
 # move the script to autostart
-echo "register to run on start up..."
 mv ./wifi_bt_auto_off.py $HOME/.config/autostart/
 
 # set the permission to executable
-echo "setting executable permission..."
 chmod u+x $HOME/.config/autostart/wifi_bt_auto_off.py
 
 # start the script
-echo "starting the program..."
 $HOME/.config/autostart/wifi_bt_auto_off.py&
 
 # final
