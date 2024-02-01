9e069751fd2e87d99a3564a6c2ff0aadca90aa0d
czhang03 <zcsx.ok@outlook.com>
2024-01-31 19:57:35 -0500

    minor script fix

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd /tmp/
 
 # download the script
 echo "downloading script..."
-wget https://raw.githubusercontent.com/czhang03/wifi_bt_auto_off/main/wifi_bt_auto_off.py > /dev/null
+wget -q https://raw.githubusercontent.com/czhang03/wifi_bt_auto_off/main/wifi_bt_auto_off.py
 
 # move the script to autostart
 echo "register to run on start up..."
@@ -20,9 +20,9 @@ chmod u+x $HOME/.config/autostart/wifi_bt_auto_off.py
 
 # start the script
 echo "starting the program..."
-$HOME/.config/autostart/wifi_bt_auto_off.py &
+$HOME/.config/autostart/wifi_bt_auto_off.py&
 
 # final
 echo "Success! The program is already running, and will auto start when you login."
-echo "To stop the program, open system monitor, and kill `wifi_bt_auto_off.py`."
-echo "To delete the program, simply delete the script `$HOME/.config/autostart/wifi_bt_auto_off.py`."
+echo "To stop the program, open system monitor, and kill 'wifi_bt_auto_off.py'."
+echo "To delete the program, simply delete the script '~/.config/autostart/wifi_bt_auto_off.py'."
